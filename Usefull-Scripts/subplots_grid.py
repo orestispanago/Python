573ae2982c1ac9e6835396bde74c0dba9f44ef3e
Orestis Panagopoulos <46169788+orestispanago@users.noreply.github.com>
2019-01-15 22:56:38 +0200

    Delete subplots_grid.py

--- Usefull-Scripts/subplots_grid.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=3, colspan=1)
-ax2 = plt.subplot2grid((6,1), (3,0), rowspan=3, colspan=1,sharex=ax1)
-
-ax1.plot(df['Temp, °C'])
-# ax2.bar(df['RH, %'])
-ax2.plot(df['RH, %'])
